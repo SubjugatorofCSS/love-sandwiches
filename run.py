a199401791ba272b95b4cb9b8238612b2ffb24ba
SubjugatorofCSS Rose <george.rose@students.codeinstitute.net>
2024-07-18 10:55:34 +0000

    update worksheet

--- run.py
+++ run.py
@@ -13,13 +13,20 @@ GSPREAD_CLIENT = gspread.authorize(SCOPED_CREDS)
 SHEET = GSPREAD_CLIENT.open('love_sandwiches')
 
 def get_sales_data():
-    print("Please enter sales data from the last market.")
-    print("Data should be six numbers, separated by commas.")
-    print("Example: 10,20,30,40,50,60\n")
 
-    data_str = input("Enter your data here: ")
-    sales_data = data_str.split(",")
-    validate_data(sales_data)
+  while True:
+        print("Please enter sales data from the last market.")
+        print("Data should be six numbers, separated by commas.")
+        print("Example: 10,20,30,40,50,60\n")
+
+        data_str = input("Enter your data here: ")
+
+        sales_data = data_str.split(",")
+
+        if validate_data(sales_data):
+            print("Data is valid!")
+            break
+
 
 def validate_data(values):
     try:
@@ -31,4 +38,15 @@ def validate_data(values):
         print(f"Invalid data: {e}, please try again.\n")
 
 
+def update_sales_worksheet(data):
+    """
+    Update sales worksheet, add new row with the list data provided
+    """
+    print("Updating sales worksheet...\n")
+    sales_worksheet = SHEET.worksheet("sales")
+    sales_worksheet.append_row(data)
+    print("Sales worksheet updated successfully.\n")
+
 get_sales_data()
+sales_data = [int(num) for num in data]
+update_sales_worksheet(sales_data)
