2ba86669a5390978381123b2a7b8c938a5b100ca
SubjugatorofCSS Rose <george.rose@students.codeinstitute.net>
2024-07-25 12:30:09 +0000

    What is anything here?

--- run.py
+++ run.py
@@ -15,12 +15,7 @@ SHEET = GSPREAD_CLIENT.open('love_sandwiches')
 
 
 def get_sales_data():
-    """
-    Get sales figures input from the user.
-    Run a while loop to collect a valid string of data from the user
-    via the terminal, which must be a string of 6 numbers separated
-    by commas. The loop will repeatedly request data, until it is valid.
-    """
+
     while True:
         print("Please enter sales data from the last market.")
         print("Data should be six numbers, separated by commas.")
@@ -38,11 +33,7 @@ def get_sales_data():
 
 
 def validate_data(values):
-    """
-    Inside the try, converts all string values into integers.
-    Raises ValueError if strings cannot be converted into int,
-    or if there aren't exactly 6 values.
-    """
+
     try:
         [int(value) for value in values]
         if len(values) != 6:
@@ -56,10 +47,15 @@ def validate_data(values):
     return True
 
 
+def update_worksheet(data, worksheet):
+
+    print(f"Updating {worksheet} worksheet...\n")
+    worksheet_to_update = SHEET.worksheet(worksheet)
+    worksheet_to_update.append_row(data)
+    print(f"{worksheet} worksheet updated successfully\n")
+
 def update_sales_worksheet(data):
-    """
-    Update sales worksheet, add new row with the list data provided
-    """
+
     print("Updating sales worksheet...\n")
     sales_worksheet = SHEET.worksheet("sales")
     sales_worksheet.append_row(data)
@@ -67,9 +63,7 @@ def update_sales_worksheet(data):
 
 
 def update_surplus_worksheet(data):
-    """
-    Update surplus worksheet, add new row with the list data provided
-    """
+
     print("Updating surplus worksheet...\n")
     surplus_worksheet = SHEET.worksheet("surplus")
     surplus_worksheet.append_row(data)
@@ -77,13 +71,7 @@ def update_surplus_worksheet(data):
 
 
 def calculate_surplus_data(sales_row):
-    """
-    Compare sales with stock and calculate the surplus for each item type.
 
-    The surplus is defined as the sales figure subtracted from the stock:
-    - Positive surplus indicates waste
-    - Negative surplus indicates extra made when stock was sold out.
-    """
     print("Calculating surplus data...\n")
     stock = SHEET.worksheet("stock").get_all_values()
     stock_row = stock[-1]
@@ -97,14 +85,13 @@ def calculate_surplus_data(sales_row):
 
 
 def main():
-    """
-    Run all program functions
-    """
+
     data = get_sales_data()
     sales_data = [int(num) for num in data]
-    update_sales_worksheet(sales_data)
+    update_worksheet(sales_data, "sales")
     new_surplus_data = calculate_surplus_data(sales_data)
-    update_surplus_worksheet(new_surplus_data)
+    update_worksheet(new_surplus_data, "surplus")
+
 
 
 print("Welcome to Love Sandwiches Data Automation")
